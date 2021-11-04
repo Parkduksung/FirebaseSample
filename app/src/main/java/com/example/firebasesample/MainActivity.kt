994e14e8895f7f315957669321fd6db8bbf3938c
Parkduksung <duksung1234@naver.com>
2021-11-04 16:35:07 +0900

    [feature] firestore CRUD. 여기서 핵심은 addSnapshot 이게 중요할듯.

--- app/src/main/java/com/example/firebasesample/MainActivity.kt
+++ app/src/main/java/com/example/firebasesample/MainActivity.kt
@@ -1,17 +1,19 @@
 package com.example.firebasesample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.EmailAuthProvider
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
 
     private var auth: FirebaseAuth? = null
+
+    private var firestore: FirebaseFirestore? = null
 
     //현재 로그인 상태인지 확인하는 리스너.
     private val authListener = FirebaseAuth.AuthStateListener {
@@ -33,6 +35,185 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         auth = FirebaseAuth.getInstance()
+
+        firestore = FirebaseFirestore.getInstance()
+
+        addSnapshotDocument()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            deleteData()
+        }, 2000L)
+
+//        firestore?.collection("User")?.document("document1")?.set(UserDTO("박", "인천", 10))
+    }
+
+
+    //중복되지는 않는다.
+    //입력방식 collection(Collection 이름).document(Document 이름).set(입력할 데이터)
+    private fun createData() {
+
+        val userDTO = UserDTO("박덕성", "인천")
+
+        firestore?.collection("User")?.document("document1")?.set(userDTO)
+            ?.addOnCompleteListener {  //이 리스너는 생략해도 상관은 없단다..
+                if (it.isSuccessful) {
+                    Log.d("결과", "데이터 등록 o")
+                } else {
+                    Log.d("결과", "데이터 등록 x")
+                }
+            }
+    }
+
+    //입력방식 collection(Collection 이름).document(Document 이름).get(입력할 데이터)
+    private fun getData() {
+
+        firestore?.collection("User")?.document("document1")?.get()?.addOnCompleteListener {
+            if (it.isSuccessful) {
+                val userDTO = it.result?.toObject(UserDTO::class.java)
+                println(userDTO)
+            }
+        }
+
+    }
+
+    //runTransaction 방식 - 여러 클라이언트의 데이터 중복 접근 방지.
+    // A가 Documents 에 접근하고 있을때에 B가 Documents 에 접근시 사용할 수 없게 된다.
+    private fun runTransaction() {
+        var tsDoc = firestore?.collection("User")?.document("document1")
+        firestore?.runTransaction { transition ->
+            val userDTO = transition.get(tsDoc!!).toObject(UserDTO::class.java)
+            userDTO?.name = "AA"
+            transition.set(tsDoc, userDTO!!)
+        }
+    }
+
+    //쿼리 방식 데이터 읽어오기.
+    // firebase 는 like 안된다.
+    private fun getQuery() {
+
+        //given
+        firestore?.run {
+            collection("User").document().set(UserDTO("박", "인천", 10))
+            collection("User").document().set(UserDTO("덕", "서울", 20))
+            collection("User").document().set(UserDTO("성", "부천", 30))
+        }
+
+
+        firestore?.collection("User")?.whereEqualTo("address", "인천")?.get()
+            ?.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    for (dc in task.result!!.documents) {
+
+                        var userDTO = dc.toObject(UserDTO::class.java)
+                        println(userDTO)
+
+                    }
+                }
+            }
+
+    }
+
+    private fun readQueryWhereGreaterThanData() {
+
+        firestore?.collection("User")?.whereGreaterThan("age", 15)?.get()
+            ?.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+
+                    for (dc in task.result!!.documents) {
+                        var userDTO = dc.toObject(UserDTO::class.java)
+                        println(userDTO)
+                    }
+
+                }
+            }
+
+    }
+
+    private fun readQueryWhereGreaterThanEqualData() {
+
+        firestore?.collection("User")?.whereGreaterThanOrEqualTo("age", 10)?.get()
+            ?.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+
+                    for (dc in task.result!!.documents) {
+                        var userDTO = dc.toObject(UserDTO::class.java)
+                        println(userDTO)
+                    }
+
+                }
+            }
+
+    }
+
+    private fun readQueryWhereLessThanData() {
+
+        firestore?.collection("User")?.whereLessThan("age", 15)?.get()
+            ?.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+
+                    for (dc in task.result!!.documents) {
+                        var userDTO = dc.toObject(UserDTO::class.java)
+                        println(userDTO)
+                    }
+
+                }
+            }
+
+    }
+
+    private fun readQueryWhereLessThanOrEqualToData() {
+
+        firestore?.collection("User")?.whereLessThanOrEqualTo("age", 10)?.get()
+            ?.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+
+                    for (dc in task.result!!.documents) {
+                        var userDTO = dc.toObject(UserDTO::class.java)
+                        println(userDTO)
+                    }
+
+                }
+            }
+    }
+
+    //감지할때 사용.
+    // 채팅할때 좋겠군..
+    private fun addSnapshotDocument() {
+        firestore?.collection("User")?.document("document1")
+            ?.addSnapshotListener { documentSnapshot, error ->
+
+                var document = documentSnapshot?.toObject(UserDTO::class.java)
+
+                Log.d("결과", "addSnapshotDocument $document")
+            }
+    }
+
+    private fun updateData() {
+
+        val map = mutableMapOf<String, Any>()
+
+        map["name"] = "박덕성입니다."
+
+        firestore?.collection("User")?.document("document1")
+            ?.update(map)?.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+
+                    Log.d("결과", "update 성공.")
+
+                }
+            }
+
+    }
+
+    private fun deleteData() {
+
+        firestore?.collection("User")?.document("document1")?.delete()
+            ?.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Log.d("결과", "삭제 성공.")
+                }
+            }
+
     }
 
 
@@ -139,3 +320,5 @@ class MainActivity : AppCompatActivity() {
         auth?.removeAuthStateListener(authListener)
     }
 }
+
+data class UserDTO(var name: String? = null, var address: String? = null, var age: Int? = null)
